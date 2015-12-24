9ab14ae1559be3398beea693c5a999c3f3153905
Prashant Shilimkar <prashant.shilimkar@neptune-int.com>
2015-12-24 16:16:37 +0530

    spring-boot started

---
+++ src/main/java/com/example/DmnDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmnDemoApplication.class, args);
+    }
+}
